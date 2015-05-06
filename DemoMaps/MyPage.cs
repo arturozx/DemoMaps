a5dc6e64a1159c51e7b47b677c2f42ebe405b663
arturozx <arturozx@gmail.com>
2015-05-06 17:39:18 -0500

    Initial check-in of module DemoMaps

---
+++ DemoMaps/MyPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace DemoMaps
+{
+	public class MyPage : ContentPage
+	{
+		public MyPage ()
+		{
+			var map = new Map(
+				MapSpan.FromCenterAndRadius(
+					new Position(21.103344699999997,-101.64351599999999), Distance.FromMiles(0.3))) {
+				IsShowingUser = true,
+				HeightRequest = 100,
+				WidthRequest = 960,
+				VerticalOptions = LayoutOptions.FillAndExpand
+			};
+
+			var position = new Position(21.103344699999997,-101.64351599999999); // Latitude, Longitude
+			var pin = new Pin {
+				Type = PinType.Place,
+				Position = position,
+				Label = "I'm here"
+			};
+			map.Pins.Add(pin);
+
+			var stack = new StackLayout { Spacing = 0 };
+			stack.Children.Add(map);
+			Content = stack;
+		}
+	}
+}
+
+
